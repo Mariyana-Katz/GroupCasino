a5c8de905c5bdd39f73b3412184a153259f3a94a
Abukarabukare <Abukarshegow@gmail.comm>
2024-03-20 12:57:18 -0400

    try

---
+++ src/main/java/com/github/zipcodewilmington/casino/games/Trivia/Trivia.java
@@ -0,0 +1,12 @@
+package com.github.zipcodewilmington.casino.games.Trivia;
+
+public class Trivia {
+
+    public String getPlayerAnswer(String playerAnswer) {
+        return playerAnswer;
+    }
+
+    public String checkAnswer(String question) {
+        return 1=1;
+    }
+}
