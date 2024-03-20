b51d95d81b92f862a9d79c9616682adc278291d3
ShailyGH <shailykunal@.com>
2024-03-20 13:57:59 -0400

    Created Interfaces.

---
+++ src/main/java/com/github/zipcodewilmington/casino/Gambler.java
@@ -0,0 +1,9 @@
+package com.github.zipcodewilmington.casino;
+
+public interface Gambler
+{
+    void withdraw(Integer amount);
+
+
+    void deposit(Integer amount);
+}
---
+++ src/test/java/com/github/zipcodewilmington/BlackjackTest.java
@@ -0,0 +1,12 @@
+package com.github.zipcodewilmington;
+
+import org.junit.Test;
+
+public class BlackjackTest
+{
+    @Test
+    public void dealCardTest()
+    {
+        GamblingPlayer player = new GamblingPlayer("Mills");
+    }
+}
