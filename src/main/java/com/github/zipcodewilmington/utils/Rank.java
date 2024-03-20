2107ade970086550b43c219afa269fab9f3549ba
Angelinamathew <angelinamathew707@gmail.com>
2024-03-19 21:42:22 -0400

    Created card,rank, suit class

---
+++ src/main/java/com/github/zipcodewilmington/utils/Rank.java
@@ -0,0 +1,33 @@
+package com.github.zipcodewilmington.utils;
+
+public enum Rank {
+    ACE(14,"A"),
+    KING(13,"K"),
+    QUEEN(12, "Q"),
+    JACK(11, "J"),
+    TEN(10, "T"),
+    NINE(9,"9"),
+    EIGHT(8, "8"),
+    SEVEN(7, "7"),
+    SIX(6, "6"),
+    FIVE(5, "5"),
+    FOUR(4, "4"),
+    THREE(3, "3"),
+    TWO(2, "2");
+
+    private int value;
+    private String faceValue;
+
+    Rank(int value, String faceValue) {
+        this.value = value;
+        this.faceValue = faceValue;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getFaceValue() {
+        return faceValue;
+    }
+}
